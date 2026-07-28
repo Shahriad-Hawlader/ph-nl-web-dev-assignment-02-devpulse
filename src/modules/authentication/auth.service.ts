e8a46fdb60fe5e8d94876c56cb9e6de1ba63a59c
Shahriad Hawlader <shahriadsaad2516@gmail.com>
2026-07-29 04:18:18 +0600

    Implement user login with JWT authentication

--- src/modules/authentication/auth.service.ts
+++ src/modules/authentication/auth.service.ts
@@ -1,15 +1,20 @@
+import config from "../../config";
 import { pool } from "../../db";
-import type { IUser } from "./auth.interface";
+import type { IUser, IUserLogin } from "./auth.interface";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 const registerUserIntoDB = async (payload: IUser) => {
   const { name, email, password, role } = payload;
+
+  const hashedPassword = await bcrypt.hash(password, 10);
 
   const result = await pool.query(
     `
     INSERT INTO users(name, email, password, role) VALUES($1,$2,$3,COALESCE($4,'contributor'))
     RETURNING *
     `,
-    [name, email, password, role],
+    [name, email, hashedPassword, role],
   );
 
   delete result.rows[0].password;
@@ -17,6 +22,44 @@ const registerUserIntoDB = async (payload: IUser) => {
   return result;
 };
 
+const loginUserIntoDB = async (payload: IUserLogin) => {
+  const { email, password } = payload;
+
+  const userData = await pool.query(
+    `
+    SELECT * FROM users WHERE email=$1
+    `,
+    [email],
+  );
+
+  if (userData.rows.length === 0) {
+    throw new Error("Invalid credentials!");
+  }
+
+  const user = userData.rows[0];
+
+  const comparePassword = await bcrypt.compare(password, user.password);
+
+  if (!comparePassword) {
+    throw new Error("Invalid credentials!");
+  }
+
+  const jwtPayload = {
+    id: user.id,
+    name: user.name,
+    role: user.role,
+  };
+
+  const token = jwt.sign(jwtPayload, config.secret as string, {
+    expiresIn: "1d",
+  });
+
+  delete user.password;
+
+  return { token, user };
+};
+
 export const authService = {
   registerUserIntoDB,
+  loginUserIntoDB,
 };

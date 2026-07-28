8b2870e3f74c337b7a470c3bd55c330b92613cab
Shahriad Hawlader <shahriadsaad2516@gmail.com>
2026-07-28 19:50:05 +0600

    Create database schema(create users table and issues table)

--- src/server.ts
+++ src/server.ts
@@ -14,6 +14,45 @@ const pool = new Pool({
   connectionString: config.connection_string,
 });
 
+const initDB = async () => {
+  try {
+    await pool.query(`
+    CREATE TABLE IF NOT EXISTS users(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(254) UNIQUE NOT NULL,
+    password TEXT NOT NULL,
+    role VARCHAR(20) DEFAULT 'contributor' CHECK (role IN ('contributor', 'maintainer')), 
+
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW()
+    )
+    `);
+
+    await pool.query(
+      `
+      CREATE TABLE IF NOT EXISTS issues(
+      id SERIAL PRIMARY KEY,
+      title VARCHAR(150) NOT NULL,
+      description VARCHAR(20) NOT NULL,
+      type VARCHAR(20) NOT NULL CHECK (type IN ('bug', 'feature_request')),
+      status VARCHAR(20) DEFAULT 'open' 
+        CHECK (status IN ('open', 'in_progress', 'resolved')),
+      reporter_id INT UNIQUE REFERENCES users(id) ON DELETE CASCADE,
+      
+      created_at TIMESTAMP DEFAULT NOW(),
+      updated_at TIMESTAMP DEFAULT NOW()
+      )
+      `,
+    );
+    console.log("DATABASE connected successfully.");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+initDB();
+
 app.get("/", (req: Request, res: Response) => {
   res.status(200).json({
     message: "DevPulse Express Server",

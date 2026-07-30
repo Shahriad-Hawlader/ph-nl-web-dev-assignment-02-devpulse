64712a1c317f79e85b1b2817df60af13a6d8622e
Shahriad Hawlader <shahriadsaad2516@gmail.com>
2026-07-31 03:57:40 +0600

    Modify query for description in issues table according to the requirement

--- src/db/index.ts
+++ src/db/index.ts
@@ -25,7 +25,8 @@ export const initDB = async () => {
       CREATE TABLE IF NOT EXISTS issues(
       id SERIAL PRIMARY KEY,
       title VARCHAR(150) NOT NULL,
-      description VARCHAR(20) NOT NULL,
+      description TEXT NOT NULL
+        CHECK (LENGTH(description) >= 20),
       type VARCHAR(20) NOT NULL CHECK (type IN ('bug', 'feature_request')),
       status VARCHAR(20) DEFAULT 'open' 
         CHECK (status IN ('open', 'in_progress', 'resolved')),
